2dce48f896cbfaecd2abfdcd9af2bdd04cd58325
felipendc <felipendc10@gmail.com>
2020-01-18 10:13:55 -0300

    Install-Fonts Script Created

---
+++ fonts/install-fonts.sh
@@ -0,0 +1,10 @@
+############################
+# Script made by Felipe Ndc
+############################
+
+# To install the fonts:
+
+sudo cp *ttf /usr/share/fonts/
+
+# To refresh the cache
+fc-cache -fv
